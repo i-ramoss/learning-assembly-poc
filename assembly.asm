977f14685574cdaf2da31985449c378da9ccf57c
Ian Ramos <ianramossantos@hotmail.com>
2023-05-15 11:28:53 -0300

    feat: add PoC of assembly program using C lang

---
+++ assembly.asm
@@ -0,0 +1,9 @@
+bits 64
+
+section .text
+
+global assembly
+
+assembly:
+  mov eax, 777
+  ret
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int assembly(void);
+
+int main(void) {
+  printf("Result: %d\n", assembly());
+
+  return 0;
+}
