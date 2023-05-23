98734b01b2191eb12ef8b878c60fbfcb584a0147
Ian Ramos <ianramossantos@hotmail.com>
2023-05-23 16:12:32 -0300

    feat: understanding nasm preprocessors like macros

--- assembly.asm
+++ assembly.asm
@@ -1,20 +1,18 @@
 bits 64 ; diretiva que especifica o tamanho das instruções seguintes (12, 32 ou 64 bits)
 
-global defVar
-global undefVar
-
-section .data
-  defVar dd 777
-
-section .bss
-  undefVar: resd 1
-
 section .text
   global assembly
 
+  %define test mov eax, 38
+
+  %macro sum5 0
+    mov ebx, 5
+    add eax, ebx
+  %endmacro
+
   assembly:
-    add dword [defVar], 3
-    mov dword [undefVar], 500
-    mov eax, 4 << 1
+    xor eax, eax
+    test
+    sum5
 
     ret
--- main.c
+++ main.c
@@ -4,18 +4,7 @@
 // recebe como retorno valor armazenado no registrador eax
 int assembly(void);
 
-extern int defVar;
-extern int undefVar;
-
 int main(void) {
-  printf("value of defVar before assembly function execution: %d\n", defVar);
-  printf("value of undefVar before assembly function execution: %d\n", undefVar);
-
-  assembly();
-
-  printf("defVar value: %d\n", defVar);
-  printf("undefVar value: %d\n", undefVar);
-
   printf("Return: %d\n", assembly());
 
   return 0;
