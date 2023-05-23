f9fb077abe7db0717ad09dfc33b745502a14d3b7
Ian Ramos <ianramossantos@hotmail.com>
2023-05-23 15:51:32 -0300

    feat: understanding variables on nasm

--- assembly.asm
+++ assembly.asm
@@ -1,10 +1,20 @@
 bits 64 ; diretiva que especifica o tamanho das instruções seguintes (12, 32 ou 64 bits)
 
-extern number
+global defVar
+global undefVar
+
+section .data
+  defVar dd 777
+
+section .bss
+  undefVar: resd 1
 
 section .text
   global assembly
+
   assembly:
-    call number
-    add eax, 111
+    add dword [defVar], 3
+    mov dword [undefVar], 500
+    mov eax, 4 << 1
+
     ret
--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int assembly(void);  // recebe como retorno valor armazenado no registrador eax
+// função definida no arquivo assembly.asm e que será acessada quando os códigos objetos desses dois arquivos forem linkados
+// recebe como retorno valor armazenado no registrador eax
+int assembly(void);
+
+extern int defVar;
+extern int undefVar;
 
 int main(void) {
-  printf("Result: %d\n", assembly());
+  printf("value of defVar before assembly function execution: %d\n", defVar);
+  printf("value of undefVar before assembly function execution: %d\n", undefVar);
+
+  assembly();
+
+  printf("defVar value: %d\n", defVar);
+  printf("undefVar value: %d\n", undefVar);
+
+  printf("Return: %d\n", assembly());
 
   return 0;
-}
-
-int number(void) {
-  return 777;
 }
