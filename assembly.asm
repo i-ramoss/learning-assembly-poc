7ebabace7c3345a65e5e5891705793d9d3a27c00
Ian Ramos <ianramossantos@hotmail.com>
2023-05-23 16:21:33 -0300

    chore: testing syscalls

--- assembly.asm
+++ assembly.asm
@@ -3,16 +3,9 @@ bits 64 ; diretiva que especifica o tamanho das instruções seguintes (12, 32 o
 section .text
   global assembly
 
-  %define test mov eax, 38
-
-  %macro sum5 0
-    mov ebx, 5
-    add eax, ebx
-  %endmacro
-
   assembly:
-    xor eax, eax
-    test
-    sum5
+    mov rax, 60
+    mov rdi, 12
+    syscall
 
     ret
