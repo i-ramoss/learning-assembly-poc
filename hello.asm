52d13b82bd5649054a3b76a0a6bfadd40a5fd1ea
Ian Ramos <ianramossantos@hotmail.com>
2023-05-23 16:50:05 -0300

    feat: hello world in assembly

---
+++ hello.asm
@@ -0,0 +1,19 @@
+bits 64
+
+section .rodata
+  msg: db `Hello World!\n`
+  MSG_SIZE equ $-msg
+
+section .text
+global _start
+
+_start:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg
+  mov rdx, MSG_SIZE
+  syscall
+
+  mov rax, 60
+  xor rdi, [MSG_SIZE]
+  syscall
